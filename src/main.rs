f538a94be33ba0032d50dc23e648e8d7497014c1
atdeters <sreted@outlook.com>
2025-11-21 22:18:08 +0100

    feat: Add color to char in correct pos

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,30 @@
 // use std::collections::HashSet;
 
-fn main()
-{
+fn main() {
     // Create a buffer to store our inputs
     let mut buffer: [[char; 5]; 6] = [['_'; 5]; 6];
 
     // TODO: Get word of the day
     // let mut _wordlist: HashSet<String> = HashSet::new();
 
-    let _word_to_find: &'static str = "harsh";
+    let word_to_find: &'static str = "harsh";
 
-    buffer[1][1] = 'a';
+    buffer[1][2] = 'a';
 
-    for elem in buffer
-    {
-        // println!("{:?}", elem);
+    for i in 0..5 {
 
-        for char in elem
-        {
+        // Check current word
+        for j in 0..4 {
             // TODO: 1. Check if the character matches the word_to_find
                 // if yes: print green
                 // if no:
                     // if does exist: print yellow
                     // else: print gray
-            print!("{}", char);
+            if word_to_find.chars().nth(j) == Some(buffer[i][j]) {
+                print!("\x1b[1;32m");
+            }
+            print!("{}", buffer[i][j]);
+            print!("\x1b[0m");
             print!(" ");
         }
         println!("");
--- target/debug/deps/wordle-15202ee7f483f4f5.d
+++ target/debug/deps/wordle-15202ee7f483f4f5.d
@@ -1,5 +1,5 @@
-/sgoinfre/adeters/ft_wordle/wordle/target/debug/deps/wordle-15202ee7f483f4f5.d: src/main.rs
+/sgoinfre/adeters/ft_wordle/target/debug/deps/wordle-15202ee7f483f4f5.d: src/main.rs
 
-/sgoinfre/adeters/ft_wordle/wordle/target/debug/deps/wordle-15202ee7f483f4f5: src/main.rs
+/sgoinfre/adeters/ft_wordle/target/debug/deps/wordle-15202ee7f483f4f5: src/main.rs
 
 src/main.rs:
