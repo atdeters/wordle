0c0f0de20fdcb4bdf439542672c9c39e024bff73
atdeters <sreted@outlook.com>
2025-11-21 20:50:11 +0100

    feat: Add buffer to store user input

--- main.rs
+++ main.rs
@@ -1,4 +1,23 @@
 fn main()
 {
-    println("Hello world");
+    // Create a buffer to store our inputs
+    let mut buffer: [[char; 5]; 6] = [['0'; 5]; 6];
+
+    let word_to_find: &'static str = "harsh";
+
+    buffer[1][1] = 'a';
+
+    for elem in buffer
+    {
+        // println!("{:?}", elem);
+        for char in elem
+        {
+            print!("{}", char);
+            print!(" ");
+        }
+        println!("");
+    }
+
+    println!("Random element is {}", buffer[1][1]);
+    println!("The word to find is {}", word_to_find);
 }
